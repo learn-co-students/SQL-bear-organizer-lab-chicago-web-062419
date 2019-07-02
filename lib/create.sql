131b39adec6e98f60d7b9316db48e0abe7222d1a
github username <github email address>
2019-07-01 22:40:28 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,19 @@
+CREATE TABLE bears (
+    id	INTEGER PRIMARY KEY, 
+    name TEXT, 
+    age INTEGER,
+    gender	TEXT,
+    color	TEXT,
+    temperament	TEXT,
+    alive BOOLEAN);
+
+
+
+
+-- SELECT name, age FROM bears WHERE gender = "female";
+
+-- SELECT name FROM bears ORDER BY name ASC;
+
+-- SELECT name,age FROM bears WHERE alive = true ORDER BY age DESC;
+
+    
